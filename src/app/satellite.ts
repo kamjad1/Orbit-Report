5763e9e767e800b1b3b126f429783d6af53dbe02
kamjad1 <73908177+kamjad1@users.noreply.github.com>
2020-12-14 09:16:07 -0600

    Adding Sattellite class

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -1,17 +1,22 @@
-export class Satellite {
+export class Satellite { 
     name: string;
-orbitType: string;
-type: string;
-operational: boolean;
-launchDate: string;
+    orbitType: string;
+    type: string;
+    operational: boolean;
+    launchDate: string;
 
-constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean) {
-    this.name = name;
-    this.type = type;
-    this.launchDate = launchDate;
-    this.orbitType = orbitType;
-    this.operational = operational;
+    constructor(name: string, orbitType: string, type: string, operational: boolean, launchDate: string) {
+        //constructor(name, orbitType, type, operational, launchDate) {
+        this.name = name;
+        this.orbitType = orbitType;
+        this.type = type;
+        this.operational = operational;
+        this.launchDate = launchDate;
+    }
+
+    shouldShowWarning(): boolean {
+       return this.type === "Space Debris";
+
+    }
 }
 
-
-}
