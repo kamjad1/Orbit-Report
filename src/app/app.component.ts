3bb270a63a0bcab1b53600cf51ae80d4919425e3
kamjad1 <73908177+kamjad1@users.noreply.github.com>
2020-12-14 19:07:34 -0600

    final changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -41,21 +41,8 @@ export class AppComponent {
           matchingSatellites.push(this.sourceList[i]);
        }
     }
-    // assign this.displayList to be the array of matching satellites
-    // this will cause Angular to re-make the table, but now only containing matches
     this.displayList = matchingSatellites;
   }
 
 }
-
-//   constructor() {
-
-//     this.sourceList = [
-//        new Satellite("SiriusXM", "Communication", "2009-03-21", "LOW", true),
-//        new Satellite("Cat Scanner", "Imaging", "2012-01-05", "LOW", true),
-//        new Satellite("Weber Grill", "Space Debris", "1996-03-25", "HIGH", false),
-//        new Satellite("GPS 938", "Positioning", "2001-11-01", "HIGH", true),
-//        new Satellite("ISS", "Space Station", "1998-11-20", "LOW", true),
-//     ];
-//  }
 
